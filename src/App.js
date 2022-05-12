b41db5aba0292bde591945656f93f1aec254bb88
BloodySteps <codewithtyson@gmail.com>
2022-05-12 16:59:52 +0100

    🔧 CONFIG: absolute paths

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import {Home, Notfound} from './pages'
+import {Home, Notfound} from 'pages'
 
 export default function App() {
   return (
