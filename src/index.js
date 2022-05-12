971756b609c6524ff2af6056a42f5c1eb0bd7b38
BloodySteps <codewithtyson@gmail.com>
2022-05-12 20:44:06 +0100

    🚧 PROGRESS: banner+search

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
-import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
 import {QueryClient, QueryClientProvider} from 'react-query'
 import {ReactQueryDevtools} from 'react-query/devtools'
 import './styles/main.css'
 import './styles/reset.css'
-import {BiSearchAlt2} from "react-icons/bi"
-import {MdArrowRightAlt} from "react-icons/md"
 
 import App from './App'
 
@@ -14,10 +11,8 @@ const root = ReactDOM.createRoot(document.getElementById('root'))
 const queryClient = new QueryClient()
 
 root.render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools position="bottom-right" />
-      <App />
-    </QueryClientProvider>
-  </StrictMode>,
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools position="bottom-right" />
+    <App />
+  </QueryClientProvider>,
 )
