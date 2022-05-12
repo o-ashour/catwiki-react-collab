a2b71e34e28a3d441f149190578b43eebc6fba34
BloodySteps <codewithtyson@gmail.com>
2022-05-12 18:07:53 +0100

    ➕ ADD DEP: react query

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client'
-import './styles/reset.css'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
 import './styles/main.css'
+import './styles/reset.css'
 
 import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
+
+const queryClient = new QueryClient()
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools position="bottom-right" />
+      <App />
+    </QueryClientProvider>
+  </StrictMode>,
 )
