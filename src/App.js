59af24d66bfb7697546525708c2321e686348803
BloodySteps <codewithtyson@gmail.com>
2022-05-12 16:55:20 +0100

    🔥 Feat: starter-setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import {Home, Notfound} from './pages'
 
 export default function App() {
-  return <div>App</div>
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </Router>
+  )
 }
